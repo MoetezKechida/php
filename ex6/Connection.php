b6b7d9d49ebe33ac5ba260fde349a6bccf8e957a
Selmi Med Dhia <monjihidousi@gmail.com>
2025-04-06 02:55:48 +0100

    added database auto-creation

--- ex6/Connection.php
+++ ex6/Connection.php
@@ -10,6 +10,9 @@ class Connection{
 
     private function __construct(){
         try{
+            self::$bdd = new PDO("mysql:host=" . self::$host . ";port=" . self::$port, self::$user, self::$pwd);
+            self::$bdd->exec("CREATE DATABASE IF NOT EXISTS `" . self::$dbname . "` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+
             self::$bdd = new PDO("mysql:host=" . self::$host . ";port=".self::$port." ;dbname=" . self::$dbname . ";charset=utf8", self::$user, self::$pwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8'));
         } catch (PDOException $e){
             die('Erreur : ' . $e->getMessage());
@@ -22,6 +25,33 @@ class Connection{
             self::create_tables();
         }
         return (self::$bdd);
+    }
+    private static function create_tables(){
+        $request = "create table if not exists utilisateur(
+            id int auto_increment primary key,
+            username varchar(20),
+            email varchar(50),
+            password varchar(100),
+            role ENUM('admin', 'user')
+        );";
+        self::$bdd->query($request);
+
+        $request = "create table if not exists section(
+            id int auto_increment primary key,
+            designation varchar(50),
+            description varchar(255)
+        );";
+        self::$bdd->query($request);
+
+        $request = "create table if not exists etudiant(
+            id int auto_increment primary key,
+            name varchar(20),
+            birthday date,
+            image varchar(50),
+            section int,
+            constraint fk_etudiant_section foreign key(section) references section(id)
+        );";
+        self::$bdd->query($request);
     }
 }
 ?>
